668e50d55aa2a3f77ea569101585d3f24ab4303a
mq <1962976634@qq.com>
2019-10-24 10:36:44 +0800

    大学生新闻中心随机述职顺序

---
+++ nepu_random.py
@@ -0,0 +1,11 @@
+from random import shuffle
+
+name = ["马琦", "郭星怡", "柏乐", "陈丹", "陈子煊", "孔书晗", "李鑫", "桑怡琳", "王昊",
+        "唐薪媛", "林琳", "刘永", "崔若楠", "崔智鑫", '向波', "辛千一"]
+shuffle(name)
+for i, j in enumerate(name):
+    name[i] = (i + 1).__str__() + ':' + j.__str__()
+# print(name)
+print("---述职随机顺序---")
+for i in name:
+    print(i)
